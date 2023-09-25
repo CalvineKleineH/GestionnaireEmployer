060fa45b9297c212f0f044c2bd5147f6117d7be4
CalvineKleineH <132929963+CalvineKleineH@users.noreply.github.com>
2023-09-25 11:12:14 +0200

    correction de quelques erreurs pour améliorer l'expérience utilisateur

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
-		
+#include <windows.h>
+#include <stdlib.h>
 
 using namespace std;
 
-const int MAX_EMPLOYER = 100;
+constexpr int MAX_EMPLOYER = 100; // Nombre maximum d'employés
 
-int salaires[MAX_EMPLOYER] = {0};
+int salaires[MAX_EMPLOYER] = {0}; 
 string noms[MAX_EMPLOYER] = {""};
 int nbEmployers = 0; // Initialisation du nombre d'employés
 
@@ -36,23 +37,30 @@ void ajouterEmployer(string nomEmployer, int salaireEmployer)
 
 void afficherDetailsEmployer(string nomEmployer)
 {
-    
+    bool estEmployer = false;
     for ( int i = 0; i < nbEmployers; i++)
     {   
-        if ( nomEmployer == noms[i])
+	// Si l'employé est dans la base donnée on affiche ses informations 
+        if ( nomEmployer == noms[i]) 
         {
             cout << "\nEmployer " << i + 1 << " : " << noms[i] << endl;
             cout << "Son salaire est de : " << salaires[i] << " euros\n" << endl;
+            estEmployer = true;
             break;
         }
-        
 
     }
-
+    // Si l'employé n'est dans la base donnée on affiche un message d'erreur
+    if (!estEmployer)
+    {
+        cout << "L'employer " << nomEmployer << " n'est pas dans votre base de donnée !" << endl;
+    }
+    
 }
 
 void calculerMoyenneSalaires()
 {
+    /*Cette fonction affiche la moyenne des salaires des employés*/
     int totalEmployers = nbEmployers;
 
     int salaireTotal = 0;
@@ -68,53 +76,66 @@ void calculerMoyenneSalaires()
 }
 
 int main()
-{
+{   
+    SetConsoleOutputCP(CP_UTF8);// Permet l'utilisation des accents dans l'invites des commandes 
     bool is_on = true; //Permet l'exécution continue du programme
     int salaire; // Permet l'entrée des salaires qui ne sont pas encore dans la base de donnée
     string nom; // Permet l'entrée des noms qui ne sont pas encore dans la base de donnée
     /*salaires et noms, sont les tableaux qui vont permettre de sauvegarder les informations des salariers
     Il sont définie or de la boucle pour éviter qui soint rénitialiser à chaque itérations.*/
-    int choix;
-
+    string choix;
+    
     while (is_on)
     {   
+        cout << "---------------------------------------------- Gestionnaire d'employés ----------------------------------------------" << endl;
         cout << "Que voulez vous faire :  \n1.Entrez un nouvel employer.\n2.voir les informations d'un employer.\n3.Afficher la moyenne des salaires.\n4.Quitter le gestionnaire." << endl;
         cout << "Choix : "; cin >> choix;
 
-        if (choix == 1)
-        {
-            /*Permet d'ajouter un employé dans la base de donnée*/
-            cout << "\nEntrez le nom de l'employer a ajouter : "; cin >> nom;
+        if (choix == "1") //Permet d'ajouter un employé dans la base de donnée
+        {  
+             system("cls");
+            cout << "\nEntrez le nom de l'employer à ajouter : "; cin >> nom;
             cout << "Entrez son salaire : "; cin >> salaire;
             cout << "" << endl;
-            if (salaire > 0) //Lorsque on n'a un salaire strictement possitif on est sur qu'un employé a été rentrer
+            if (salaire > 0 && nom != "") //Lorsque on n'a un salaire strictement possitif on est sur qu'un employé a été rentrer
             {
                 nbEmployers += 1;
                 ajouterEmployer(nom, salaire);
                 /*------------------------------------------------------*/
             }
-            else
+	    /*Message d'erreur au cas au l'utilisateur rentre un salaire nul ou négatif*/
+            else if (nom.size() == 0)
             {
-                cout << "Erreur !" << endl;
+                cout << "Erreur au niveau du nom rentré !\n" << endl;
+            }
+            else if (salaire <= 0)
+            {
+                cout << "Erreur au niveau du salaire rentré !\n" << endl;
             }
                 
         }
-        else if (choix == 2)
-        {
+        else if (choix == "2") // Affichage des détails d'un enmployé
+        {  
+	    system("cls");
             string employer;
             cout << "\nEntrer le nom de l'employer : "; cin >> employer;
             afficherDetailsEmployer(employer);
         }
-        else if (choix == 3)
-        {
+        else if (choix == "3")//Affichage de la moyenne des salaires 
+        {   system("cls");
             calculerMoyenneSalaires();
             
         }
-        else if (choix == 4)
+        else if (choix == "4")//Quitte le programme
         {
             is_on = false;
         }
-        
+        else 
+        {   system("cls");// Message d'erreur si l'utilisateur rentre une entrée invalide 
+            cout << "Entrez un nombre valide !\n" << endl;
+            
+        }
+       
         
 
 
